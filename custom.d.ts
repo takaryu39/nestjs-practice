56f61b5d5991c4eb7127d3053b326413f7931174
github takaryu39 <ryuta0takamiya@gmail.com>
2023-09-14 06:42:36 +0900

    認証のapi作成

---
+++ custom.d.ts
@@ -0,0 +1,8 @@
+import { User as CustomUser } from '@prisma/client';
+declare global {
+  namespace Express {
+    export interface Request {
+      user: CustomUser;
+    }
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,25 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UpdateUserDto } from './dto/update-user.dso';
 import { UserService } from './user.service';
+import { User } from '@prisma/client';
+import { Request } from 'express';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+  @Get()
+  getLoginUser(@Req() req: Request): Omit<User, 'hashedPassword'> {
+    return req.user;
+  }
+
+  @Patch()
+  updateUser(
+    @Req() req: Request,
+    @Body() dto: UpdateUserDto,
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    return this.userService.updateUser(req.user.id, dto);
+  }
 }
